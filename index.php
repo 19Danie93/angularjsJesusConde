f6d2eb1b9ef59f47bbbf2ed8619b263da8203fcf
Luis Velasquez <l.velasquez1308@gmail.com>
2014-11-24 14:52:19 -0600

    Renombrando archivos a html

--- index.php
+++ index.php
@@ -16,11 +16,11 @@
         AngularJS fue creado por {{Creadores}} hace solo {{2014-2009}} años. <br/>
         links a tutos: <br/>
         <ul>
-            <li><a href="tuto1.php">tuto 1</a></li>
-            <li><a href="tuto2.php">tuto 2</a></li>
-            <li><a href="tuto4.php">tuto 4</a></li>
-            <li><a href="tuto5.php">tuto 5</a></li>
-            <li><a href="tuto6.php">tuto 6</a></li>
+            <li><a href="tuto1.html">tuto 1</a></li>
+            <li><a href="tuto2.html">tuto 2</a></li>
+            <li><a href="tuto4.html">tuto 4</a></li>
+            <li><a href="tuto5.html">tuto 5</a></li>
+            <li><a href="tuto6.html">tuto 6</a></li>
 
         </ul>
         <script src="js/angular.min.js" type="text/javascript"></script>
